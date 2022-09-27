57457758fe943b9c3624a1e529f245a42ace5aec
adrian7cd <adrianipsys@proton.me>
2022-09-27 21:58:21 +0200

    game logic

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 # Init pygame
 pygame.init()
@@ -14,6 +14,7 @@ clock = pygame.time.Clock()
 
 # Set game values
 SNAKE_SIZE = 20
+
 head_x = WINDOW_WIDTH//2
 head_y = WINDOW_HEIGHT//2 + 100
 
@@ -28,6 +29,7 @@ DARKGREEN = (10, 50, 10)
 RED = (255, 0, 0)
 DARKRED = (150, 0, 0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # Set fonts
 font = pygame.font.SysFont('gabriola', 48)
@@ -48,10 +50,8 @@ game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
-# Set sounds and music
-#pick_up_sound = pygame.mixer.Sound()
 
-# Set images
+# Set images5
 apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
 apple_rect = pygame.draw.rect(display, RED, apple_coord)
 
@@ -70,18 +70,76 @@ while game_running:
     if event.type == pygame.QUIT:
       game_running = False
 
-  display.fill(WHITE)
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        snake_dx = -1*SNAKE_SIZE
+        snake_dy = 0
+      if event.key == pygame.K_RIGHT:
+        snake_dx = SNAKE_SIZE
+        snake_dy = 0
+      if event.key == pygame.K_UP:
+        snake_dx = 0
+        snake_dy = -1*SNAKE_SIZE
+      if event.key == pygame.K_DOWN:
+        snake_dx = 0
+        snake_dy = SNAKE_SIZE
 
-  # HUD
+  body_coords.insert(0, head_coord)
+  body_coords.pop()
+
+  head_x += snake_dx
+  head_y += snake_dy
+  head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+
+  if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+      display.blit(game_over_text, game_over_rect)
+      display.blit(continue_text, continue_rect)
+      pygame.display.update()
+
+      is_paused = True
+      while is_paused:
+        for event in pygame.event.get():
+          if event.type == pygame.KEYDOWN:
+            score = 0
+
+            head_x = WINDOW_WIDTH//2
+            head_y = WINDOW_HEIGHT//2 + 100
+            head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+                    
+            body_coords = []
+
+            snake_dx = 0
+            snake_dy = 0
+
+            is_paused = False
+          if event.type == pygame.QUIT:
+            is_paused = False
+            running = False
+
+  if head_rect.colliderect(apple_rect):
+    score += 1
+
+    apple_x = random.randint(0, WINDOW_WIDTH - SNAKE_SIZE)
+    apple_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
+    apple_coord = (apple_x, apple_y, SNAKE_SIZE, SNAKE_SIZE)
+
+    body_coords.append(head_coord)
+
+  score_text = font.render("Score: " + str(score), True, GREEN, DARKRED)
+
+  display.fill(BLACK)
+    
   display.blit(title_text, title_rect)
   display.blit(score_text, score_rect)
 
-  pygame.draw.rect(display, GREEN, head_coord)
-  pygame.draw.rect(display, RED, apple_coord)
+  for body in body_coords:
+      pygame.draw.rect(display, DARKGREEN, body)
+  head_rect = pygame.draw.rect(display, GREEN, head_coord)
+  apple_rect = pygame.draw.rect(display, RED, apple_coord)
 
-  # Update display
+    #Update display and tick clock
   pygame.display.update()
   clock.tick(FPS)
 
-# END GAME
+#End the game
 pygame.quit()
